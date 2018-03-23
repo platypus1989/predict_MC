99c57611134c7cda73527eec6096e8170f9b8ff5
Chuan Wang <wangchuan1989@gmail.com>
2018-03-22 22:44:22 -0400

    build a minimal python module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='predict_MC',
+      version='0.1.0',
+      description='Missing Value Imputation in Prediction by Monte Carlo Method',
+      url='https://github.com/platypus1989/predict_MC',
+      author='platypus',
+      author_email='wangchuan1989@gmail.com',
+      license='MIT',
+      packages=['predict_MC'],
+      zip_safe=False)
